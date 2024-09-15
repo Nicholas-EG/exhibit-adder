6157e28a68badae6a5cc280735a19db27ab52f55
Nicholas <141982119+Nicholas-EG@users.noreply.github.com>
2024-09-15 10:40:54 -0700

    added teh mergeCoverAndExhibits method, still has an error in it

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const { PDFDocument, StandardFonts } = require('pdf-lib');
+const { PDFDocument, StandardFonts } = require('pdf-lib'); // api available at https://pdf-lib.js.org/docs/api/
 const fs = require('fs');
 
 async function makeCoverPage(name) {
   // set up document 
   const pdfDoc = await PDFDocument.create();
   const page = pdfDoc.addPage();
-  const timesFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  const timesFont = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
   page.setFont(timesFont);
 
   // write on page
@@ -27,9 +27,11 @@ function run(numberOfExhibits) {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   for(let i = 0; i < numberOfExhibits; i++) {
     let name = '';
+
     // calculate how many letters will be in the name
     let loops = Math.ceil(i / alphabet.length) + (i % alphabet.length === 0 ? 1 : 0);
     for(let j = 0; j < loops; j++) {
+
       // calculate which letter to put in the name
       name = alphabet[i % alphabet.length] + name;
     }
@@ -37,4 +39,27 @@ function run(numberOfExhibits) {
   }
 }
 
-run(10)
+
+// proof of concept, just takes one article and creates one coversheet and merges them together
+async function mergeCoverAndExhibit(num) {
+  // TODO: make the article naming convention dynamic
+  const uint8array = fs.readFileSync('./articles/test.pdf');
+  const pdfDoc = await PDFDocument.load(uint8array);
+  run(num);
+  setTimeout(() => {console.log("check");
+  }, "2000");
+  /** FIXME: the line below generates an error if the coversheet is newly created. fs is unable to find the coversheet.
+   *  Possible that the file is not fully loaded when run exits, so fs is unable to find it. Possible fix would be
+   *  to add a short sleep() period right after run to give the computer time to finalize placing the docs.
+   */
+  // TODO: make the naming convention dynamic
+  const coverletter = fs.readFileSync('./coversheets/cover A.pdf');
+  const pdfDoc2 = await PDFDocument.load(coverletter);
+  const copiedpages = await pdfDoc2.copyPages(pdfDoc, [...Array(pdfDoc.getPageCount()).keys()]);
+  for(i in copiedpages) {
+    pdfDoc2.addPage(copiedpages[i]);
+  }
+  fs.writeFileSync('./result.pdf', await pdfDoc2.save());
+}
+
+mergeCoverAndExhibit(1);
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pdfkit
 import time
 
 start = time.perf_counter()
-pdfkit.from_url('https://google.com', './test.pdf')
+pdfkit.from_url('https://google.com', './articles/test.pdf')
 end = time.perf_counter()
 
 print(end - start)
